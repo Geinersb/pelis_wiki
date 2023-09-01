dbe22e107bce3e75518f1cde351f4ac6b7996153
Geinersb <geinersb20@gmail.com>
2023-08-31 21:26:41 -0600

    se agregan los titulos y subtitulos del listview

--- lib/presentation/widgets/movies/movie_horizontal_listview.dart
+++ lib/presentation/widgets/movies/movie_horizontal_listview.dart
@@ -1,5 +1,6 @@
 import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:pelis_wiki/config/helpers/human_fortmats.dart';
 
 import '../../../domain/entities/movie.dart';
 
@@ -90,14 +91,20 @@ class _Slide extends StatelessWidget {
           ),
 
 //* RATING
-          Row(
-            children: [
-              Icon(Icons.star_half_outlined, color: Colors.yellow.shade800),
-            const   SizedBox(width: 3),
-              Text('${movie.voteAverage}',style: textStyles.bodyMedium?.copyWith(color: Colors.yellow.shade800)),
-               const   SizedBox(width: 10),
-               Text('${movie.popularity}',style: textStyles.bodySmall),
-            ],
+          SizedBox(
+            width: 150,
+            child: Row(
+              children: [
+                Icon(Icons.star_half_outlined, color: Colors.yellow.shade800),
+              const   SizedBox(width: 3),
+                Text('${movie.voteAverage}',style: textStyles.bodyMedium?.copyWith(color: Colors.yellow.shade800)),
+             //    const   SizedBox(width: 10),
+          const Spacer(),
+                 Text(HumanFormats.number(movie.popularity),style: textStyles.bodySmall)
+          
+                
+              ],
+            ),
           )
         ],
       ),
