ac0cbea303d06645fe65bf513056e5c165b43573
Geinersb <geinersb20@gmail.com>
2023-10-27 20:53:00 -0600

    se implementa el Isar Datasource

--- lib/infraestructure/datasources/isar_datasource.dart
+++ lib/infraestructure/datasources/isar_datasource.dart
@@ -16,28 +16,44 @@ class IsarDatasource extends LocalStorageDatasource {
     if (Isar.instanceNames.isEmpty) {
       return await Isar.open(
         [MovieSchema],
-         inspector: true,
-         directory: dir.path,
-         );
+        inspector: true,
+        directory: dir.path,
+      );
     }
     return Future.value(Isar.getInstance());
   }
 
   @override
-  Future<bool> isMovieFavorite(int movieId) {
-    // TODO: implement isMovieFavorite
-    throw UnimplementedError();
+  Future<bool> isMovieFavorite(int movieId) async {
+    final isar = await db;
+
+    final Movie? isFavoriteMovie =
+        await isar.movies.filter().idEqualTo(movieId).findFirst();
+
+    return isFavoriteMovie != null;
   }
 
   @override
-  Future<void> toggleFavorite(Movie movie) {
-    // TODO: implement toggleFavorite
-    throw UnimplementedError();
+  Future<void> toggleFavorite(Movie movie) async {
+    final isar = await db;
+
+    final favoriteMovie =
+        await isar.movies.filter().idEqualTo(movie.id).findFirst();
+
+    if (favoriteMovie != null) {
+      //borrar
+       isar.writeTxnSync(() => isar.movies.deleteSync(favoriteMovie.isarId!));
+      return;
+    }
+
+//insertar
+    isar.writeTxnSync(() => isar.movies.putSync(movie));
   }
 
   @override
-  Future<List<Movie>> loadMovies({int limit = 10, offset = 0}) {
-    // TODO: implement loadMovies
-    throw UnimplementedError();
+  Future<List<Movie>> loadMovies({int limit = 10, offset = 0}) async {
+    final isar = await db;
+
+    return isar.movies.where().offset(offset).limit(limit).findAll();
   }
 }
