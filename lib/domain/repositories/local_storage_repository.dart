a3b2d02174917c5beccf32c792feeba850627f69
Geinersb <geinersb20@gmail.com>
2023-10-27 19:40:57 -0600

    se realiza configuracion para usar el Isar DB

---
+++ lib/domain/repositories/local_storage_repository.dart
@@ -0,0 +1,17 @@
+
+
+import 'package:pelis_wiki/domain/entities/movie.dart';
+
+abstract class LocalStorageRepository{
+
+ Future<void> toggleFavorite(Movie movie);
+
+  Future<bool> isMovieFavorite(int movieId);
+
+  Future<List<Movie>> loadMovies({int limit = 10, offset = 0});
+
+  
+
+
+
+}
---
+++ lib/infraestructure/datasources/isar_datasource.dart
@@ -0,0 +1,43 @@
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:pelis_wiki/domain/datasources/local_storage_datasource.dart';
+import 'package:pelis_wiki/domain/entities/movie.dart';
+
+class IsarDatasource extends LocalStorageDatasource {
+  late Future<Isar> db;
+
+  IsarDatasource() {
+    db = openDB();
+  }
+
+  Future<Isar> openDB() async {
+    final dir = await getApplicationDocumentsDirectory();
+
+    if (Isar.instanceNames.isEmpty) {
+      return await Isar.open(
+        [MovieSchema],
+         inspector: true,
+         directory: dir.path,
+         );
+    }
+    return Future.value(Isar.getInstance());
+  }
+
+  @override
+  Future<bool> isMovieFavorite(int movieId) {
+    // TODO: implement isMovieFavorite
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> toggleFavorite(Movie movie) {
+    // TODO: implement toggleFavorite
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Movie>> loadMovies({int limit = 10, offset = 0}) {
+    // TODO: implement loadMovies
+    throw UnimplementedError();
+  }
+}
